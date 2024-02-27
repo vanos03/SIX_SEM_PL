c6353e782e21f2300a5089cd9f376d396a7358ec
Your Name <you@example.com>
2024-02-27 06:56:21 -0500

    first commit

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ template <class T>
 void test_pq(){
     PQ<T> pq(5);
     // pq.get_size();
-    // if (pq.is_empty() == 0) std::cout << "empty";
+    pq.is_empty();
     pq.insert(1);
     pq.insert(1);
     // if (pq.is_empty()) std::cout << "empty";
--- priority_queue.cpp
+++ priority_queue.cpp
@@ -12,6 +12,25 @@ class PQ{
             b = temp;
         }
 
+        void sort_heap(int i){
+            unsigned int left = 2*i+1;
+            unsigned int right = 2*i+2; 
+
+            if (left < this->size){
+                if (this->mas[i] < this->mas[left]){
+                    swap(this->mas[i], this->mas[left]);
+                    sort_heap(left);
+                }
+            }
+
+            if (right < this->size){
+                if (this->mas[i] < this->mas[right]){
+                    swap(this->mas[i], this->mas[right]);
+                    sort_heap(right);
+                }
+            }
+        }
+
     public:
         PQ(int size){
             this->size = 0;
@@ -23,17 +42,16 @@ class PQ{
             else return 1;
         }
 
-        int get_size(){
-            return this->size;
-        }
+        int get_size(){return this->size;}
 
         void insert(T new_item){
             unsigned int i = this->size;
             unsigned int parent = (i-1)/2;
+            this->mas[i] = new_item;
 
             while (parent >= 0 && i > 0){
                 if(this->mas[i] > this->mas[parent])
-                    swap(mas[i], mas[parent]);
+                    swap(this->mas[i], mas[parent]);
 
                 i = parent;
                 parent = (i-1)/2;
@@ -42,12 +60,30 @@ class PQ{
             this->size++;
         }
 
-        void del_item(){}
+        void del_max(){
+            this->mas[0] = this->mas[this->size-1];
+            this->size--;
+            sort_heap(0);
+        }
 
-        T get_max(){}
+        T get_max(){return this->mas[0];}
 
         void print(){
-            
+            unsigned int i = 0;
+            unsigned int k = 1;
+
+            // for (int i = 0; i < this->size; i++)
+            //     std::cout << this->mas[i] << " ";
+            // std::cout << std::endl;
+
+            while (i < this->size){
+                while((i < k) && (i < this->size)){
+                    std::cout << this->mas[i] << " ";
+                    i++;
+                }
+                std::cout << std::endl;
+                k = k*2+1;
+            }
         }
 
         ~PQ(){
@@ -55,3 +91,27 @@ class PQ{
             delete [] this->mas;
         }
 };
+
+
+#define N 100
+
+template <class T>
+void test_pq(){
+    PQ<T> pq(N);
+
+    for (int i = 1; i < N; i++)
+        pq.insert(i);
+
+    pq.print();
+    std::cout << std::endl;
+    pq.del_max();
+    pq.print();
+
+
+}
+
+int main(){
+    
+    test_pq<int>();
+    return 0;
+}
