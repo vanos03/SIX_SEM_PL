445563810db08539f9e6d78b3c1c1cb44c3cc543
ivan123456 <48185734+vanos03@users.noreply.github.com>
2024-04-09 20:33:06 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <stdexcept>
+
+#include "map.cpp"
+
+int main() {
+    Hash_map<int, int> map;
+    dict_t<int, int> d;
+
+    for (int i = 0; i < 3; i++){
+        map.insert(i, i);
+        map.insert(i*2, i*2);
+        map.insert(i*5, i*5);
+    }
+    std::cout << "size: " << map.get_size() << std::endl;
+    map.print_vec();
+    map.del_el(1);
+    std::cout << std::hash<int>{}(1) << std::endl;
+    std::cout << "size: " << map.get_size() << std::endl;
+    map.print_vec();
+    // map.del_all();
+    return 0;
+}
